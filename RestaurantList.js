91e7576ae0b804e3693a3e8be2e60dd3cb5549bc
Adam Lawson <alaws057@uottawa.ca>
2019-06-27 13:54:32 -0400

    Added some initial code and features to get us started

--- RestaurantList.js
+++ RestaurantList.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {Header} from 'react-native';
+import { Card, ListItem, Button, Icon } from 'react-native-elements'
 
-export default class RestaurantList extends React.Component {
+export class RestaurantList extends React.Component {
+    static navigationOptions = ({ navigation }) => {
+        return {
+          title: 'Restaurants',
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            marginRight: '40%',
+            marginLeft: 'auto',
+          },
+        };
+      };
     render(){
         return (
             <View style={styles.container}>
-            <Text>RestaurantPage!</Text>
+              <Card
+                title="McDonald's">
+                <Text style={{marginBottom: 10}}>
+                    The idea with React Native Elements is more about component structure than actual design.
+                </Text>
+                <Button
+                    icon={<Icon name='code' color='#ffffff' />}
+                    backgroundColor='#03A9F4'
+                    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                    title='VIEW NOW' />
+                </Card>  
             </View>
         );
     }
@@ -19,3 +45,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default RestaurantList;
